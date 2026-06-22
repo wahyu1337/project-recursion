c9fa45bc74dbf43ddeed3f3b96f19722ec22c2ae
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-06-22 23:03:35 +0800

    finishing recursion merge (app.js)

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
-function merge (start, mid, end) {
-    const merged = [];
+function merge (arr, l, mid, h) {
+    const leftArr = arr.slice(l, mid + 1);
+    const rightArr = arr.slice(mid + 1, h + 1);
+
+    // pointer for comparing two element
+    let i = 0;
+    let j = 0;
+    let k = l;
+
+    while (i < leftArr.length && j < rightArr.length) {
+        // comparison logic
+        if (leftArr[i] <= rightArr[j]) {
+            // push the left side element if it's same
+            arr[k] = leftArr[i];
+            i++; // move forward
+            k++; // move forward the k also
+        } else {
+            arr[k] = rightArr[j];
+            j++; // move forward
+            k++; // move forward
+        }
+    }
+
+    // clean the element if there still any left (left sub)
+    while (i < leftArr.length){
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    } 
+
+    // clean the element if there still any left (right sub)
+    while(j < rightArr.length) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+    return arr;
 }
 
 function mergeSort (arr = [], l = 0, h = arr.length - 1) {
@@ -14,11 +49,10 @@ function mergeSort (arr = [], l = 0, h = arr.length - 1) {
     // process
     mergeSort(arr, l, mid);
     mergeSort(arr, mid + 1, h);
-    console.log(arr);
 
-    // return merge(l, mid, h);
+    return merge(arr, l, mid, h);
 }
 console.log(mergeSort([]));
 console.log(mergeSort([73]));
-// console.log(mergeSort([1, 2, 3, 4, 5]));
+console.log(mergeSort([1, 2, 3, 4, 5]));
 console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
